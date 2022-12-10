166fe29e659c55e357ab731b148508d7b9877951
urieisenberg <urieisenberg@gmail.com>
2022-12-10 20:28:30 +0200

    google maps integration with ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@
 
 import { User } from './User';
 import { Company } from './Company';
+import { Map } from './Map';
 
 const user = new User();
 const company = new Company();
+const map = new Map('map');
 
 console.log(user);
 console.log(company);
